24bd18002e68f86804c5a127ddf81342bbddddac
Thanhle-23 <lequocquyenthanh@gmail.com>
2024-12-10 10:24:32 +0700

    Add files via upload

---
+++ bai 25 buoi 9.cpp
@@ -0,0 +1,68 @@
+
+#include<stdio.h>
+#include<string.h>
+typedef struct {
+	char mamon[10];
+	float diem;
+}MON_HOC;
+
+typedef struct {
+	char hoten[30], diachi[100], gioitinh[4];
+	int ngaysinh, mssv, somon;
+	MON_HOC monhoc[10];
+} SINH_VIEN SV;
+
+void nhap(SINH_VIEN *sv);
+void xuat(SINH_VIEN sv);
+int main()
+{
+	SINH_VIEN sv;
+	
+	nhap(&sv);
+	
+	printf("\nThong tin vua nhap la: \n");
+	xuat(sv);
+	
+	return 0;
+}
+
+void xuat(SINH_VIEN sv)
+{
+	printf("%s\t%s\t%s\t%d\t%d\t", sv.hoten, sv.diachi, sv.gioitinh, sv.ngaysinh, sv.somon);
+	for(int i=0; i<sv.somon; i++){
+		printf("Ma mon: %s\n Diem: %f", sv.monhoc[i].mamon, sv.monhoc[i].diem);
+	}
+}
+void nhap(SINH_VIEN *sv)
+{
+	//Nhap thanh phan ho ten
+	printf("\nNhap vao ho ten: ");
+	fflush(stdin);
+	gets(sv->hoten);
+	
+	//Nhap thanh phan dia chi
+	printf("\nNhap vao dia chi: ");
+	fflush(stdin);
+	gets(sv->diachi);
+	
+	//Nhap thanh phan gioi tinh
+	printf("\nNhap vao gioi tinh: ");
+	fflush(stdin);
+	gets(sv->gioitinh);
+	
+	//Nhap thanh phan ngay sinh
+	printf("\nHay cho biet ngay sinh: ");
+	scanf("%d", &sv->ngaysinh);
+	
+	//Nhap thanh phan mon hoc
+	printf("\nNhap vao so mon hoc: ");
+	fflush(stdin);
+	scanf("%d",&sv->somon);
+	
+	for(int i=0; i < sv->somon; i++){
+		printf("Nhap ma mon hoc thu %d ", i+1);
+		scanf("%s",sv->monhoc[i].mamon);
+		printf("Nhap diem mon thu %d ", i+1);
+		scanf("%f",sv->monhoc[i].diem);
+	}
+}
