9a697ef0c446dee8f5603ad133371b2220658fca
Thanhle-23 <lequocquyenthanh@gmail.com>
2024-12-12 07:57:11 +0700

    Update bai 23 buoi 8.cpp

--- bai 23 buoi 8.cpp
+++ bai 23 buoi 8.cpp
@@ -1,52 +1,69 @@
-#include<string.h>
-#include<stdio.h>
-struct CON_NGUOI{
-	char hoten[30], diachi[100], gioitinh[4];
-	int tuoi;
-	float thunhap;
-};
-typedef struct CON_NGUOI CN;
 
-void nhap(CN *a);
-void xuat(CN a);
+#include<stdio.h>
+#include<string.h>
+
+struct MON_HOC{
+	char mamon[10];
+	float diem;
+};
+
+struct SINH_VIEN{
+	char hoten[30], diachi[100], gioitinh[4];
+	int ngaysinh, mssv, somon;
+	MON_HOC monhoc[10];
+} ;
+typedef struct SINH_VIEN sv;
+
+void nhap(SINH_VIEN *sv);
+void xuat(SINH_VIEN sv);
 int main()
 {
-	CON_NGUOI a, b;
+	SINH_VIEN sv;
 	
-	nhap(&a);
+	nhap(&sv);
 	
 	printf("\nThong tin vua nhap la: \n");
-	xuat(a);
+	xuat(sv);
 	
 	return 0;
 }
-void xuat(CN a)
+
+void xuat(SINH_VIEN sv)
 {
-	printf("%s\t%s\t%s\t%d\t%f\t", a.hoten, a.diachi, a.gioitinh, a.tuoi, a.thunhap);
+	printf("%s\t%s\t%s\t%d\t%d\t", sv.hoten, sv.diachi, sv.gioitinh, sv.ngaysinh, sv.somon);
+	for(int i=0; i<sv.somon; i++){
+		printf("Ma mon: %s\n Diem: %f", sv.monhoc[i].mamon, sv.monhoc[i].diem);
+	}
 }
-void nhap(CN *a)
+void nhap(SINH_VIEN *sv)
 {
 	//Nhap thanh phan ho ten
 	printf("\nNhap vao ho ten: ");
 	fflush(stdin);
-	gets(a->hoten);
+	gets(sv->hoten);
 	
 	//Nhap thanh phan dia chi
 	printf("\nNhap vao dia chi: ");
 	fflush(stdin);
-	gets(a->diachi);
+	gets(sv->diachi);
 	
 	//Nhap thanh phan gioi tinh
 	printf("\nNhap vao gioi tinh: ");
 	fflush(stdin);
-	gets(a->gioitinh);
+	gets(sv->gioitinh);
 	
-	//Nhap thanh phan tuoi
-	printf("\nHay cho biet tuoi: ");
-	scanf("%d", &a->tuoi);
+	//Nhap thanh phan ngay sinh
+	printf("\nHay cho biet ngay sinh: ");
+	scanf("%d", &sv->ngaysinh);
 	
-	//Nhap thanh phan thu nhap
-	printf("\nNhap vao thu nhap: ");
-	fflush(stdin);
-	scanf("%f",&a->thunhap);
+	//Nhap thanh phan mon hoc
+	printf("\nNhap vao so mon hoc: ");
+	scanf("%d",&sv->somon);
+	
+	for(int i=0; i < sv->somon; i++){
+		printf("Nhap ma mon hoc thu %d ", i+1);
+		scanf("%s",sv->monhoc[i].mamon);
+		printf("Nhap diem mon thu %d ", i+1);
+		scanf("%f",sv->monhoc[i].diem);
+	}
 }
