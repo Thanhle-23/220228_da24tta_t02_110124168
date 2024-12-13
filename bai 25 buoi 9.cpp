c7ebd09601fe126f6da0691ccf1b06339a45aba5
Thanhle-23 <lequocquyenthanh@gmail.com>
2024-12-13 15:35:32 +0700

    Update bai 25 buoi 9.cpp

--- bai 25 buoi 9.cpp
+++ bai 25 buoi 9.cpp
@@ -1,6 +1,13 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+
+struct NGAY_THANG{
+	int ngay;
+	int thang;
+	int nam;
+};
 
 struct MON_HOC{
 	char mamon[10];
@@ -8,8 +15,9 @@ struct MON_HOC{
 };
 
 struct SINH_VIEN{
-	char hoten[30], diachi[100], gioitinh[4];
-	int ngaysinh, mssv, somon;
+	char hoten[30], diachi[100], gioitinh[4], mssv[10];
+	int somon;
+	NGAY_THANG ngaysinh;
 	MON_HOC monhoc[10];
 } ;
 typedef struct SINH_VIEN sv;
@@ -17,6 +25,7 @@ typedef struct SINH_VIEN sv;
 void nhap(SINH_VIEN *sv);
 void xuat(SINH_VIEN sv);
 void nhapnsv(SINH_VIEN *sv);
+void timSVmssv(SINH_VIEN *ds, int n, char mssv);
 int main()
 {
 	SINH_VIEN sv;
@@ -27,13 +36,30 @@ int main()
 	xuat(sv);
 	
 	nhapnsv(&sv);
+	
+	char mssv_tim[10];
+    printf("\nNhap MSSV can tim: ");
+    scanf("%s", mssv_tim);
+
+    SINH_VIEN *ket_qua = timSVmssv(danh_sach, so_luong, mssv_tim);
+    if (ket_qua != NULL) {
+        printf("\nThong tin sinh vien tim thay:\n");
+        xuat(ket_qua);
+    } else {
+        printf("\nKhong tim thay sinh vien co MSSV %s.\n", mssv_tim);
+    }
+
+    for (int i = 0; i < so_luong; i++) {
+        free(danh_sach[i].mon_hoc);
+    }
+    free(danh_sach);
 
 	return 0;
 }
 
 void xuat(SINH_VIEN sv)
 {
-	printf("%s\t%s\t%s\t%d\t%d\t", sv.hoten, sv.diachi, sv.gioitinh, sv.ngaysinh, sv.somon);
+	printf("%s\t%s\t%s\t%s\t%d\t%d\t", sv.hoten, sv.mssv, sv.diachi, sv.gioitinh, sv.ngaysinh, sv.somon);
 	for(int i=0; i<sv.somon; i++){
 		printf("Ma mon: %s\n Diem: %f", sv.monhoc[i].mamon, sv.monhoc[i].diem);
 	}
@@ -44,6 +70,10 @@ void nhap(SINH_VIEN *sv)
 	printf("\nNhap vao ho ten: ");
 	fflush(stdin);
 	gets(sv->hoten);
+	
+	//Nhap thanh phan mssv
+	printf("\nHay cho biet mssv: ");
+	scanf("%s", &sv->mssv);
 	
 	//Nhap thanh phan dia chi
 	printf("\nNhap vao dia chi: ");
@@ -57,7 +87,7 @@ void nhap(SINH_VIEN *sv)
 	
 	//Nhap thanh phan ngay sinh
 	printf("\nHay cho biet ngay sinh: ");
-	scanf("%d", &sv->ngaysinh);
+	scanf("%d %d %d", &sv->ngaysinh.ngay, &sv->ngaysinh.thang, &sv->ngaysinh.nam);
 	
 	//Nhap thanh phan mon hoc
 	printf("\nNhap vao so mon hoc: ");
@@ -70,18 +100,26 @@ void nhap(SINH_VIEN *sv)
 		scanf("%f",&sv->monhoc[i].diem);
 	}
 }
-void nhapnsv(SINH_VIEN *sv){
+void nhapnsv(SINH_VIEN *ds, int *n){
 		int n;
 	printf("\nnhap so luong sinh vien: ");
 	scanf("%d",&n);
-	SINH_VIEN dsSV[n];
-	for(int i=0; i<n;i++){
+	*ds=(SINH_VIEN*)malloc((*n)*sizeof(SINH_VIEN));
+	for(int i=0; i<*n;i++){
 		printf("\nnhap thong tin sinh vien thu %d: ",i+1);
-		nhap(&dsSV[i]);
+		nhap(&(*ds)[i]);
 	}
 	printf("\n THong tin danh sach sinh vien");
 	for(int i=0; i<n; i++){
 		printf("\nSinh vien thu %d: ",i+1);
-		xuat(dsSV[i])	;
+		xuat(&ds[i])	;
 	}
+}
+void timSVmssv(SINH_VIEN*ds, int n, char *mssv){
+	for(int i=0;i<n;i++){
+		if(strcmp(ds[i].mssv, mssv)==0){
+			return (SINH_VIEN*)&ds[i];
+		}
+	}
+	return NULL;
 }
